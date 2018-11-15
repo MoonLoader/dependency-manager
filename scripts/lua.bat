9c803cfce7beae842c161029adae2e071d6dd172
FYP <its.fyp@gmail.com>
2018-11-15 12:55:35 +0300

    Change lua paths

--- scripts/lua.bat
+++ scripts/lua.bat
@@ -5,8 +5,10 @@ CALL :NORMALIZEPATH "%~dp0..\luarocks"
 SET "LUAROCKS_PATH=%RETVAL%"
 CALL :NORMALIZEPATH "%~dp0..\lib"
 SET "LIB_PATH=%RETVAL%"
-SET "LUA_PATH=%LIB_PATH%\std\?.lua;%LIB_PATH%\std\?\init.lua;%LIB_PATH%\?.luac;%LIB_PATH%\?\init.luac;%LUAROCKS_PATH%\lua\?.lua;%LUAROCKS_PATH%\lua\?\init.lua"
-SET "LUA_CPATH=%LIB_PATH%\std\?.dll"
+CALL :NORMALIZEPATH "%~dp0..\libstd"
+SET "LIBSTD_PATH=%RETVAL%"
+SET "LUA_PATH=%LIBSTD_PATH%\?.lua;%LIBSTD_PATH%\?\init.lua;%LIB_PATH%\?.luac;%LIB_PATH%\?\init.luac;%LUAROCKS_PATH%\lua\?.lua;%LUAROCKS_PATH%\lua\?\init.lua"
+SET "LUA_CPATH=%LIBSTD_PATH%\?.dll;%LIB_PATH%\?.dll"
 ENDLOCAL & SET "LUA_PATH=%LUA_PATH%" & SET "LUA_CPATH=%LUA_CPATH%"
 
 "%~dp0\bin\luajit.exe" -lluarocks.loader %*
